085ec9239f899e86ce815396b2b69c79b47e433b
Abid Ahmad <abidahmad0025@gmail.com>
2025-08-05 15:47:12 +0600

    Update Program.cs

--- DigitalDetox.API/Program.cs
+++ DigitalDetox.API/Program.cs
@@ -24,11 +24,9 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // === Middleware ===
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
